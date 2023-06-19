4682633a16529d64d95a5e4fd6f756e2078415f8
wtnhrr <brunoqg210@gmail.com>
2023-06-19 02:13:10 -0300

    add+

--- cutBox.cpp
+++ cutBox.cpp
@@ -1,7 +1,7 @@
 #include "cutBox.h"
 
 cutBox::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
-    this -> x0 = x1, this -> x1 = x1;
+    this -> x0 = x0, this -> x1 = x1;
     this -> y0 = y0, this -> y1 = y1;
     this -> z0 = z0, this -> z1 = z1;
 }
